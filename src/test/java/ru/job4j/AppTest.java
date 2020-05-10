07e3c2565ab56656d4f272c9697eba7f5a11a2f1
Stanislav Klokov <klokovstas2013@gmail.com>
2020-05-10 10:28:28 +0300

    Travis initial

---
+++ src/test/java/ru/job4j/AppTest.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void test() {
+        App test = new App();
+        System.out.println("Test passed");
+    }
+}
